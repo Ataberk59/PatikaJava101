5b17087ad4c634ef4f821f7816c42d3f0f4d618b
Ataberk Calis <calsataberk@gmail.com>
2025-12-08 22:48:13 +0300

    KDV Hesaplayan Program Ödevi

--- src/kdvHesaplama.java
+++ src/kdvHesaplama.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class kdvHesaplama {
     public void main (String[] args){
-        System.out.println("İlk commit hayırlısı olsun");
+    double tutar;
+    double kdvliÜcret;
+    double kdv;
+    Scanner input = new Scanner(System.in);
+        System.out.println("Ücret tutarını giriniz: ");
+        tutar = input.nextDouble();
+        System.out.println("Girilen değer: " + tutar);
+        if((0 < tutar) && (tutar <= 1000) ){
+            kdv = tutar * 0.18;
+            kdvliÜcret = tutar + kdv;
+            System.out.println("KDV 'li tutar: " + kdvliÜcret);
+        }else if (1000 < tutar){
+            kdv = tutar * 0.08;
+            kdvliÜcret = (tutar + kdv);
+            System.out.println("KDV 'li tutar: " + kdvliÜcret);
+        }else {
+            System.out.println("Pozitif değer giriniz!");
+        }
     }
 }
