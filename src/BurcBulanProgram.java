54659fdb662b890a03cd38fddae1119f843bd544
Ataberk Calis <calsataberk@gmail.com>
2026-01-22 16:36:13 +0300

    Burç Program Ödevi

---
+++ src/BurcBulanProgram.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class BurcBulanProgram {
+    public static void main(String[] args){
+        int month, day;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Burcunuzu öğrenmek için doğum tarihinizin gününü giriniz: ");
+        day = input.nextInt();
+
+        System.out.println("Burcunuzu öğrenmek için doğum tarhinizin ayını giriniz: ");
+        month = input.nextInt();
+
+        switch (month){
+            case 1 :
+                if (day < 22){
+                    System.out.println("Oğlak");
+                } else {
+                    System.out.println("Kova");
+                }
+                break;
+            case 2 :
+                if (day < 20){
+                    System.out.println("Kova");
+                } else {
+                    System.out.println("Balık");
+                }
+                break;
+            case 3 :
+                if (day < 21){
+                    System.out.println("Balık");
+                } else {
+                    System.out.println("Koç");
+                }break;
+            case 4 :
+                if (day < 21){
+                    System.out.println("Koç");
+                } else {
+                    System.out.println("Boğa");
+                }
+                break;
+            case 5 :
+                if (day < 22){
+                    System.out.println("Boğa");
+                } else {
+                    System.out.println("İkizler");
+                }
+                break;
+            case 6 :
+                if (day < 23){
+                    System.out.println("İkizler");
+                } else {
+                    System.out.println("Yengeç");
+                }
+                break;
+            case 7 :
+                if (day < 23){
+                    System.out.println("Yengeç");
+                } else {
+                    System.out.println("Aslan");
+                }
+                break;
+            case 8 :
+                if (day < 23){
+                    System.out.println("Aslan");
+                } else {
+                    System.out.println("Başak");
+                }
+                break;
+            case 9 :
+                if (day < 23){
+                    System.out.println("Başak");
+                } else {
+                    System.out.println("Terazi");
+                }
+                break;
+            case 10 :
+                if (day < 22){
+                    System.out.println("Terazi");
+                } else {
+                    System.out.println("Akrep");
+                }
+                break;
+            case 11 :
+                if (day < 22){
+                    System.out.println("Akrep");
+                } else {
+                    System.out.println("Yay");
+                }
+                break;
+            case 12 :
+                if (day < 22){
+                    System.out.println("Yay");
+                } else {
+                    System.out.println("Oğlak");
+                }
+                break;
+            default:
+                System.out.println("Hatalı değer girdiniz!");
+        }
+    }
+}
